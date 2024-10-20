6df0f49bd3a01a9dc3c516952286897ca8b01362
ashish.alex10@gmail.com <ashish.alex10@gmail.com>
2024-10-20 16:41:15 +0100

    chore: more data extraction

--- model/data_from_api.py
+++ model/data_from_api.py
@@ -9,11 +9,15 @@ x_rapidapi_key = os.getenv("X-RAPIDAPI-KEY")
 
 RONALDO_PLAYER_ID = "874"
 AL_NASSR_TEAM_ID = "2939"
+SPORTING_CP_TEAM_ID = "228"
+MANCHESTER_UNITED_TEAM_ID = "33"
+JUVENTUS_TEAM_ID = "496"
+REAL_MADRID_TEAM_ID = "541"
 # # querystring = {"league":"307","season":"2024","team":"2939"}
 # querystring = {"season":"2023","team":"2939"}
 
 
-def get_fixtures_for_team(team_id:str, season:str):
+def get_fixtures(team_id:str, season:str):
     url = "https://api-football-v1.p.rapidapi.com/v3/fixtures"
 
     querystring = {"season": season, "team": team_id}
@@ -24,7 +28,8 @@ def get_fixtures_for_team(team_id:str, season:str):
         }
     response = requests.get(url, headers=headers, params=querystring)
     output = response.json()
-    write_json_to_file(output, f'data/{team_id}_fixutures_{season}.json')
+    return output
+    # write_json_to_file(output, f'data/{team_id}_fixutures_{season}.json')
 
 
 def get_player_stats_for_fixture(fixture_id:str):
@@ -77,3 +82,39 @@ def get_teams_player_has_played_for(player_id:str):
 # get_player_stats_for_fixture(fixture_id)
 # output = get_teams_player_has_played_for("874")
 # write_json_to_file(output, 'data/ronaldo_teams.json')
+# seasons = [
+#   "2021",
+#   "2008",
+#   "2007",
+#   "2006",
+#   "2005",
+#   "2004",
+#   "2003"
+# ]
+
+# seasons = [
+#   "2020",
+#   "2019",
+#   "2018"
+# ]
+
+# seasons = [
+#   "2017",
+#   "2016",
+#   "2015",
+#   "2014",
+#   "2013",
+#   "2012",
+#   "2011",
+#   "2010",
+#   "2009"
+# ]
+
+# seasons = ["2022"]
+#
+# for season in seasons:
+#     output = get_fixtures(AL_NASSR_TEAM_ID, season)
+#     write_json_to_file(output, f'data/al_nassr_fixtures_{season}.json')
+
+# output = get_fixtures(MANCHESTER_UNITED_TEAM_ID, "2023")
+# write_json_to_file(output, 'data/manchester_united_fixtures_2003.json')
