816f794e569e99bff977d3a64ec36e8f0a3b06c9
ashish.alex10@gmail.com <ashish.alex10@gmail.com>
2024-10-22 00:10:54 +0100

    feat: show of match

--- frontend/app/match.tsx
+++ frontend/app/match.tsx
@@ -1,42 +1,24 @@
-//'use client';
-//
-//import React from 'react';
-//import { Separator } from "@/components/ui/separator"
-//
-//interface MatchItemProps {
-//  match: string;
-//  onClick: (match: string) => void;
-//}
-//
-//export const MatchItem: React.FC<MatchItemProps> = ({ match, onClick }) => {
-//  return (
-//    <React.Fragment>
-//      <div
-//        className="text-sm cursor-pointer hover:bg-gray-100"
-//        onClick={() => onClick(match)}
-//      >
-//        {match}
-//      </div>
-//      <Separator className="my-2" />
-//    </React.Fragment>
-//  );
-//};
-//
-//export default MatchItem;
-//
 'use client';
 
 import React from 'react';
 import { Separator } from "@/components/ui/separator"
+import { format } from 'date-fns';
 
 interface MatchItemProps {
-  team1: string;
-  team2: string;
-  expectedGoals: number;
-  onClick: (team1: string, team2: string) => void;
+    team1: string;
+    team1_logo: string;
+    team2: string;
+    team2_logo: string;
+    goals: string;
+    date: Date;
+    status: string;
+    onClick: (team1: string, team2: string) => void;
 }
 
-export const MatchItem: React.FC<MatchItemProps> = ({ team1, team2, expectedGoals, onClick }) => {
+export const MatchItem: React.FC<MatchItemProps> = ({ team1, team1_logo, team2, team2_logo, goals, date, status, onClick }) => {
+  const formattedDate = format(date, 'EEE, d MMM');
+  const formattedTime = format(date, 'HH:mm');
+
   return (
     <React.Fragment>
       <div
@@ -47,8 +29,18 @@ export const MatchItem: React.FC<MatchItemProps> = ({ team1, team2, expectedGoal
           <span className="text-sm">{team1}</span>
           <span className="text-sm">{team2}</span>
         </div>
-        <div className="flex flex-col items-end">
-          <span className="text-sm">{expectedGoals}</span>
+        <div className="flex items-center">
+
+          { status === 'NS' ? (
+              <span className="text-sm mr-4"></span>
+          ) : (
+              <span className="text-sm mr-4">{goals}</span>
+          )}
+
+          <div className="flex flex-col items-end">
+            <span className="text-xs text-gray-500">{formattedDate}</span>
+            <span className="text-xs text-gray-500">{formattedTime}</span>
+          </div>
         </div>
       </div>
       <Separator className="my-2" />
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,18 +2,56 @@
 "use client";
 
 import * as React from "react"
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { LineComponent } from "./line";
 import { MatchItem } from "./match";
 import { RadarComponent } from "./radar";
-import { footballMatches } from "./footballMatches";
 import { TableComponent } from "./table";
 import { SofaPlayerEmbed } from './sofa';
 
+
+async function getTeamFixtures(teamId: number) {
+    const response = await fetch(`/api/get_team_fixtures?team_id=${teamId}`)
+    let output = await response.json()
+    let fixtures = [];
+
+    for (let i = 0; i < output.response.length; i++) {
+        let status = output.response[i].fixture.status.short;
+        let goals = "";
+        if ((status === "FT" || status === "HT" || status === "PN") &&  output.response[i].goals.home !== null && output.response[i].goals.away !== null) {
+            goals = output.response[i].goals.home + " - " + output.response[i].goals.away
+        }else{
+            goals = ""
+        }
+
+        fixtures.push({
+            fixture_date: output.response[i].fixture.date,
+            team1: output.response[i].teams.home.name,
+            team1_logo: output.response[i].teams.home.logo,
+            team2: output.response[i].teams.away.name,
+            team2_logo: output.response[i].teams.away.logo,
+            goals: goals,
+            status: output.response[i].fixture.status.short,
+            key: output.response[i].fixture.id
+        })
+    }
+    fixtures.sort((a, b) => new Date(a.fixture_date).getTime() - new Date(b.fixture_date).getTime())
+    return fixtures
+}
+
 export default function Home() {
-    const [selectedMatch, setSelectedMatch] = useState<string>("Al Nassr vs Al Hilal: 1");
+    const [selectedMatch, setSelectedMatch] = useState<string>("");
+    const [teamFixtures, setTeamFixtures] = useState<any>([])
+    const alNassrTeamId = 2939
+
+    useEffect(() => {
+        getTeamFixtures(alNassrTeamId).then(data => {
+            if (data) setTeamFixtures(data)
+        })
+    }, [])
+
 
 
     const handleMatchClick = (match: string) => {
@@ -28,12 +66,16 @@ export default function Home() {
             <ScrollArea className="h-100 w-80 rounded-md border">
                 <div className="p-4">
                     <h4 className="mb-4 text-sm font-bold leading-none">Matches</h4>
-                    {footballMatches.map((data) => (
+                    {teamFixtures.map((data:any) => (
                         <MatchItem
-                            key={data.team1 + " vs " + data.team2}
+                            key={data.key}
                             team1={data.team1}
+                            team1_logo={data.team1_logo}
                             team2={data.team2}
-                            expectedGoals={data.expectedGoals}
+                            team2_logo={data.team2_logo}
+                            goals={data.goals}
+                            date={data.fixture_date}
+                            status={data.status}
                             onClick={handleMatchClick}
                         />
                     ))}
