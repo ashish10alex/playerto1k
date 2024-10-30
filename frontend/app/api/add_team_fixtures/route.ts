3b2e57112332fbb638631f66b1a2d9bb98ff1610
ashish.alex10@gmail.com <ashish.alex10@gmail.com>
2024-10-30 17:25:10 +0000

    feat: support for multiple teamIds

--- frontend/app/api/add_team_fixtures/route.ts
+++ frontend/app/api/add_team_fixtures/route.ts
@@ -1,26 +1,37 @@
 import { NextResponse } from 'next/server'
 import { NextRequest } from 'next/server'
-import  prisma  from "@/lib/prisma";
+import prisma from "@/lib/prisma";
 import { Fixture } from '@/types'
 
 type FetchOptions = {
-  method: string;
-  headers: {
-    'x-rapidapi-host': string;
-    'x-rapidapi-key': string;
-  };
+    method: string;
+    headers: {
+        'x-rapidapi-host': string;
+        'x-rapidapi-key': string;
+    };
 };
 
 
-let getTeamFixtures = async (output:any): Promise<Fixture[]> => {
+let getTeamFixtures = async (season: number, teamId: number): Promise<Fixture[]> => {
+    const url = `https://api-football-v1.p.rapidapi.com/v3/fixtures?season=${season}&team=${teamId}`;
+    const options: FetchOptions = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': process.env.X_RAPIDAPI_KEY as string,
+            'x-rapidapi-host': 'api-football-v1.p.rapidapi.com'
+        }
+    };
+    const response = await fetch(url, options);
+    const output = await response.json();
+
     let fixtures = [];
 
     for (let i = 0; i < output.response.length; i++) {
         let status = output.response[i].fixture.status.short;
         let goals = "";
-        if ((status === "FT" || status === "HT" || status === "PN") &&  output.response[i].goals.home !== null && output.response[i].goals.away !== null) {
+        if ((status === "FT" || status === "HT" || status === "PN") && output.response[i].goals.home !== null && output.response[i].goals.away !== null) {
             goals = output.response[i].goals.home + " - " + output.response[i].goals.away
-        }else{
+        } else {
             goals = ""
         }
 
@@ -43,13 +54,13 @@ let getTeamFixtures = async (output:any): Promise<Fixture[]> => {
 
 
 async function addTeamFixtures(fixtureData: Fixture[]) {
-  await prisma.fixture.createMany({
+    await prisma.fixture.createMany({
         data: fixtureData,
         skipDuplicates: true,
-  })
+    })
 }
 
-async function deleteTeamFixtures(){
+async function deleteTeamFixtures() {
     const tenDaysAgo = new Date();
     tenDaysAgo.setDate(tenDaysAgo.getDate() - 10);
     const deletedRecords = await prisma.fixture.deleteMany({
@@ -64,32 +75,21 @@ async function deleteTeamFixtures(){
 
 export async function POST(request: NextRequest) {
     let body = await request.json()
-    const { teamId } = body;
+    const { teamIds } = body;
 
-    if (!teamId) {
-        return NextResponse.json({ error: 'Team ID is required' }, { status: 400 })
+    if (!teamIds) {
+        return NextResponse.json({ error: 'Team ID(s) is required' }, { status: 400 })
     }
 
-    const season = 2024; // TODO: get season from the API
-    const url = `https://api-football-v1.p.rapidapi.com/v3/fixtures?season=${season}&team=${teamId}`;
-    const options: FetchOptions = {
-        method: 'GET',
-        headers: {
-            'x-rapidapi-key': process.env.X_RAPIDAPI_KEY as string,
-            'x-rapidapi-host': 'api-football-v1.p.rapidapi.com'
-        }
-    };
-
     try {
-        const response = await fetch(url, options);
-        const output = await response.json();
-        let fixtures = await getTeamFixtures(output);
-        if (fixtures){
-            const deletedFixtures = await deleteTeamFixtures()
-            console.log(`deleted ${deletedFixtures.count} records from fixture table`)
-            await addTeamFixtures(fixtures)
+        const seasons = [2024, 2025]
+        for (let i = 0; i < teamIds.length; i++) {
+            for (let j = 0; j < seasons.length; j++) {
+                let fixtures = await getTeamFixtures(seasons[j], teamIds[i]);
+                await addTeamFixtures(fixtures)
+            }
         }
-        return NextResponse.json({ "fixtures": fixtures })
+        return NextResponse.json({ "message": "Data added successfully" }, { status: 200 })
     } catch (error) {
         console.log(error)
         return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 })
