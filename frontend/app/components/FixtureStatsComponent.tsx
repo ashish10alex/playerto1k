4e046c506731053de0f392e43e691927aa7608c6
ashish.alex10@gmail.com <ashish.alex10@gmail.com>
2024-11-01 17:45:56 +0000

    chore: fit things in a page

--- frontend/app/components/FixtureStatsComponent.tsx
+++ frontend/app/components/FixtureStatsComponent.tsx
@@ -58,9 +58,8 @@ interface FixtureStats {
 const FixtureStatsComponent: React.FC<{ stats: FixtureStats }> = ({ stats }) => {
     console.log(stats);
     return (
-        <div className="bg-gray-100 rounded-lg p-5 w-full shadow-md">
-            <h2 className="text-2xl font-bold text-center text-gray-800 mb-5">Fixture Stats</h2>
 
+        <div>
             <MatchItem
                 key={stats?.id}
                 fixtureId={stats?.id}
@@ -71,47 +70,50 @@ const FixtureStatsComponent: React.FC<{ stats: FixtureStats }> = ({ stats }) => 
                 goals={stats?.goals}
                 date={isNaN(Date.parse(stats?.date)) ? new Date() : new Date(stats?.date)}
                 status={stats?.status}
-                onClick={() => {}}
+                onClick={() => { }}
             />
 
-            <StatSection title="General">
-                <StatItem icon={<FaClock />} label="Minutes" value={stats?.playerFixtureStatistics[0]?.games_minutes} />
-                <StatItem icon={<FaTshirt />} label="Number" value={stats?.playerFixtureStatistics[0]?.games_number} />
-                <StatItem icon={<FaFutbol />} label="Position" value={stats?.playerFixtureStatistics[0]?.games_position} />
-                <StatItem icon={<FaStar />} label="Rating" value={stats?.playerFixtureStatistics[0]?.games_rating} />
-                <StatItem icon={<FaFlag />} label="Captain" value={stats?.playerFixtureStatistics[0]?.games_captain ? 'Yes' : 'No'} />
-            </StatSection>
+            <div className="bg-gray-100 rounded-lg p-2 w-full shadow-md">
 
-            <StatSection title="Goals & Shots">
-                <StatItem icon={<FaFootballBall />} label="Goals" value={stats?.playerFixtureStatistics[0]?.goals_total || 0} />
-                <StatItem icon={<FaBullseye />} label="Shots (On Target)" value={`${stats?.playerFixtureStatistics[0]?.shots_total} (${stats?.playerFixtureStatistics[0]?.shots_on})`} />
-                <StatItem icon={<FaCaretSquareRight />} label="Penalties Scored" value={stats?.playerFixtureStatistics[0]?.penalty_scored} />
-                <StatItem icon={<FaCaretSquareLeft />} label="Penalties Missed" value={stats?.playerFixtureStatistics[0]?.penalty_missed} />
-            </StatSection>
+                <StatSection title="General">
+                    <StatItem icon={<FaClock />} label="Minutes" value={stats?.playerFixtureStatistics[0]?.games_minutes} />
+                    <StatItem icon={<FaTshirt />} label="Number" value={stats?.playerFixtureStatistics[0]?.games_number} />
+                    <StatItem icon={<FaFutbol />} label="Position" value={stats?.playerFixtureStatistics[0]?.games_position} />
+                    <StatItem icon={<FaStar />} label="Rating" value={stats?.playerFixtureStatistics[0]?.games_rating} />
+                    <StatItem icon={<FaFlag />} label="Captain" value={stats?.playerFixtureStatistics[0]?.games_captain ? 'Yes' : 'No'} />
+                </StatSection>
 
-            <StatSection title="Passing">
-                <StatItem icon={<FaShareAlt />} label="Passes (accurate)" value={`${stats?.playerFixtureStatistics[0]?.passes_total} (${stats?.playerFixtureStatistics[0]?.passes_accuracy})`} />
-                <StatItem icon={<FaUserFriends />} label="Key Passes" value={stats?.playerFixtureStatistics[0]?.passes_key} />
-            </StatSection>
+                <StatSection title="Goals & Shots">
+                    <StatItem icon={<FaFootballBall />} label="Goals" value={stats?.playerFixtureStatistics[0]?.goals_total || 0} />
+                    <StatItem icon={<FaBullseye />} label="Shots (On Target)" value={`${stats?.playerFixtureStatistics[0]?.shots_total} (${stats?.playerFixtureStatistics[0]?.shots_on})`} />
+                    <StatItem icon={<FaCaretSquareRight />} label="Penalties Scored" value={stats?.playerFixtureStatistics[0]?.penalty_scored} />
+                    <StatItem icon={<FaCaretSquareLeft />} label="Penalties Missed" value={stats?.playerFixtureStatistics[0]?.penalty_missed} />
+                </StatSection>
 
-            <StatSection title="Defensive Actions">
-                <StatItem icon={<FaShieldAlt />} label="Tackles" value={stats?.playerFixtureStatistics[0]?.tackles_total} />
-                <StatItem icon={<FaExchangeAlt />} label="Interceptions" value={stats?.playerFixtureStatistics[0]?.tackles_interceptions} />
-                <StatItem icon={<FaExchangeAlt />} label="Duels (Won)" value={`${stats?.playerFixtureStatistics[0]?.duels_total} (${stats?.playerFixtureStatistics[0]?.duels_won})`} />
-            </StatSection>
+                <StatSection title="Passing">
+                    <StatItem icon={<FaShareAlt />} label="Passes (accurate)" value={`${stats?.playerFixtureStatistics[0]?.passes_total} (${stats?.playerFixtureStatistics[0]?.passes_accuracy})`} />
+                    <StatItem icon={<FaUserFriends />} label="Key Passes" value={stats?.playerFixtureStatistics[0]?.passes_key} />
+                </StatSection>
 
-            <StatSection title="Dribbling & Fouls">
-                <StatItem icon={<FaRunning />} label="Dribbles (Success)" value={`${stats?.playerFixtureStatistics[0]?.dribbles_attempts} (${stats?.playerFixtureStatistics[0]?.dribbles_success})`} />
-                <StatItem icon={<FaHandPaper />} label="Cards" value={`🟨 ${stats?.playerFixtureStatistics[0]?.cards_yellow} 🟥 ${stats?.playerFixtureStatistics[0]?.cards_red}`} />
-            </StatSection>
+                <StatSection title="Defensive Actions">
+                    <StatItem icon={<FaShieldAlt />} label="Tackles" value={stats?.playerFixtureStatistics[0]?.tackles_total} />
+                    <StatItem icon={<FaExchangeAlt />} label="Interceptions" value={stats?.playerFixtureStatistics[0]?.tackles_interceptions} />
+                    <StatItem icon={<FaExchangeAlt />} label="Duels (Won)" value={`${stats?.playerFixtureStatistics[0]?.duels_total} (${stats?.playerFixtureStatistics[0]?.duels_won})`} />
+                </StatSection>
+
+                <StatSection title="Dribbling & Fouls">
+                    <StatItem icon={<FaRunning />} label="Dribbles (Success)" value={`${stats?.playerFixtureStatistics[0]?.dribbles_attempts} (${stats?.playerFixtureStatistics[0]?.dribbles_success})`} />
+                    <StatItem icon={<FaHandPaper />} label="Cards" value={`🟨 ${stats?.playerFixtureStatistics[0]?.cards_yellow} 🟥 ${stats?.playerFixtureStatistics[0]?.cards_red}`} />
+                </StatSection>
+            </div>
         </div>
     );
 };
 
 const StatSection: React.FC<{ title: string; children: React.ReactNode }> = ({ title, children }) => (
-    <div className="mb-6">
-        <h3 className="text-xl font-semibold mb-3 text-gray-700">{title}</h3>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+    <div className="mb-2">
+        <h3 className="text-l font-semibold mb-3 text-gray-700">{title}</h3>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2">
             {children}
         </div>
     </div>
